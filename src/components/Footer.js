4e6690b2c6499b4779c0feafacb43319096d348b
sarangan <43719998+saranganES@users.noreply.github.com>
2023-08-06 23:30:47 +0530

    fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
         <>
             <div className='bg-[#051324] py-6 md:flex text-center items-center justify-center'>
                 <div className='text-white text-sm ml-3'>Copyright © 2023 CryptoBuddy</div>
-                <div className='text-white text-sm ml-3 md:block hidden'   >|</div>
+                <div className='text-[#4c4c4c] text-sm ml-3 md:block hidden'>|</div>
                 <div className='text-white text-sm ml-3'>Designed by Ezio Solutions (P) Ltd</div>
             </div>
         </>
